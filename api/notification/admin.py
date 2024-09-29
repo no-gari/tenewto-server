f8124dacc25a0a584e1f524ffa483f8fbe25adcf
no-gari <sofaissofa@icloud.com>
2024-09-29 15:04:51 +0900

    final

--- api/notification/admin.py
+++ api/notification/admin.py
@@ -2,7 +2,7 @@ from api.notification.models import Notification
 from django.contrib import admin
 
 
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    class Meta:
-        fields = ('title', 'subtitle', 'url', 'hits')
+# @admin.register(Notification)
+# class NotificationAdmin(admin.ModelAdmin):
+#     class Meta:
+#         fields = ('title', 'subtitle', 'url', 'hits')
