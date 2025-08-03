b3d4bd8aed928281eecea36fdf440adb85b6da96
no-gari <sofaissofa@icloud.com>
2025-08-03 17:00:56 +0900

    delete recommendation app

--- api/recommendation/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('daily/', views.RecommendationAPIView.as_view(), name='daily_recommendations'),
-    path('viewed/<int:user_id>/', views.mark_recommendation_viewed, name='mark_viewed'),
-    path('liked/<int:user_id>/', views.mark_recommendation_liked, name='mark_liked'),
-]
