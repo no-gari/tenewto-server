77c725f3635e7368aa8235b1cefab856f721d288
no-gari <sofaissofa@icloud.com>
2025-08-08 00:37:49 +0900

    fix serialize problem

--- api/chat/consumers.py
+++ api/chat/consumers.py
@@ -1,8 +1,8 @@
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from api.chat.models import Message, Chat
 from django.db import transaction
-import json
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -10,17 +10,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.user = self.scope['user']
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
 
-        # 채팅방이 있는지 확인
         self.chat = await self.get_chat()
-        if self.chat:
-            if self.chat and await self.check_user_in_chat():
-                await self.channel_layer.group_add(
-                    self.chat_id,
-                    self.channel_name,
-                )
-                await self.accept()
-            else:
-                await self.close()
+        if self.chat and await self.check_user_in_chat():
+            await self.channel_layer.group_add(
+                self.chat_id,
+                self.channel_name,
+            )
+            await self.accept()
         else:
             await self.close()
 
@@ -39,33 +35,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text = text_data_json['text']
         message = await self.create_message(text)
 
+        # datetime 객체를 ISO 형식 문자열로 변환하여 채널 레이어로 전송
         await self.channel_layer.group_send(
             self.chat_id,
             {
                 'type': 'send_message',
                 'user': message.user.pk,
                 'text': message.text,
-                'created': message.created,
+                'created': message.created.isoformat(),
             },
         )
 
     async def send_message(self, event):
-        user = event['user']
-        text = event['text']
-        created = event['created']
+        # 채널 레이어에서 받은 이벤트는 이미 직렬화 가능한 데이터
         await self.send(text_data=json.dumps({
-            'user': user,
-            'text': text,
-            'created': created,
+            'user': event['user'],
+            'text': event['text'],
+            'created': event['created'],
         }))
 
     @database_sync_to_async
     def get_chat(self):
         try:
-            chat = Chat.objects.prefetch_related('user_set').get(pk=self.chat_id)
+            return Chat.objects.prefetch_related('user_set').get(pk=self.chat_id)
         except Chat.DoesNotExist:
-            chat = None
-        return chat
+            return None
 
     @database_sync_to_async
     @transaction.atomic
@@ -79,4 +73,3 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.chat.updated = message.created
         self.chat.save()
         return message
-
